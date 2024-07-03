786632221e0aed06eb1b5580e4a9e1af8e43c117
Lokesh Kumar <lokeshkumar@Lokeshs-MacBook-Pro.local>
2024-07-03 12:26:44 +0530

    handle adminlogin hashed password

--- src/app/Admin/page.tsx
+++ src/app/Admin/page.tsx
@@ -1,9 +1,10 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FIRESTORE_DB } from "../../../firebase.config";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, getDoc, doc, updateDoc } from "firebase/firestore";
 import { FaHome } from "react-icons/fa";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 function Admin() {
   const [itemName, setItemName] = useState("");
@@ -11,20 +12,33 @@ function Admin() {
   const [imageUrl, setImageUrl] = useState("");
   const [price, setPrice] = useState("");
   const [gender, setGender] = useState("");
-  const [category, setcategory] = useState("");
+  const [category, setCategory] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchAdminStatus = async () => {
+      try {
+        const docRef = doc(FIRESTORE_DB, "admincred", "admincred"); // Replace with actual document ID
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+          const { loggedIn } = docSnap.data();
+          setLoggedIn(loggedIn);
+        } else {
+          console.log("No such document!");
+        }
+      } catch (error) {
+        console.error("Error fetching document:", error);
+      }
+    };
+
+    fetchAdminStatus();
+  }, []);
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
     try {
-      // Logging input values for debugging
-      console.log("Submitting with:", {
-        itemName,
-        sizes,
-        imageUrl,
-        price,
-        gender,
-      });
-
       const docRef = await addDoc(collection(FIRESTORE_DB, "items"), {
         name: itemName,
         price: price,
@@ -35,15 +49,27 @@ function Admin() {
       });
       alert("Item added Successfully!!");
       console.log("Item added with ID:", docRef.id);
-      // Optionally reset state after successful submission
       setItemName("");
       setSizes("");
       setImageUrl("");
       setPrice("");
       setGender("");
-      setcategory("");
+      setCategory("");
     } catch (error) {
       console.error("Error adding item:", error);
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      const docRef = doc(FIRESTORE_DB, "admincred", "admincred"); // Replace with actual document ID
+      await updateDoc(docRef, {
+        loggedIn: false,
+      });
+      setLoggedIn(false);
+      router.push("/AdminLogin");
+    } catch (error) {
+      console.error("Error updating document:", error);
     }
   };
 
@@ -62,128 +88,149 @@ function Admin() {
           <FaHome size={34} color="white" />
         </div>
       </Link>
-      <form
-        onSubmit={handleSubmit}
-        className="w-full max-w-md bg-gray-100 rounded-lg shadow-lg p-6"
-      >
-        <h2 className="text-2xl mb-4 text-center">Add New Shoe Item</h2>
-        <div className="mb-4">
-          <label
-            htmlFor="itemName"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Shoe Name:
-          </label>
-          <input
-            type="text"
-            id="itemName"
-            value={itemName}
-            onChange={(e) => setItemName(e.target.value)}
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="sizes"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Sizes Available:
-          </label>
-          <input
-            type="text"
-            id="sizes"
-            value={sizes}
-            onChange={(e) => setSizes(e.target.value)}
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="imageUrl"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Image URL:
-          </label>
-          <input
-            type="url"
-            id="imageUrl"
-            value={imageUrl}
-            onChange={(e) => setImageUrl(e.target.value)}
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="price"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Price:
-          </label>
-          <input
-            type="number"
-            id="price"
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="gender"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Gender:
-          </label>
-          <select
-            id="gender"
-            value={gender}
-            onChange={(e) => setGender(e.target.value)}
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            required
-          >
-            <option value="">Select gender</option>
-            <option value="Men">Men</option>
-            <option value="Women">Women</option>
-            <option value="Kids">Kids</option>
-          </select>
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="category"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Category:
-          </label>
-          <select
-            id="Category"
-            value={category}
-            onChange={(e) => setcategory(e.target.value)}
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            required
-          >
-            <option value="">Select Category</option>
-            <option value="Athletic">Athletic</option>
-            <option value="Casual">Casual</option>
-            <option value="Dress">Dress</option>
-            <option value="Boots">Boots</option>
-            <option value="Sandals">Sandals</option>
-            <option value="Specialty">Specialty</option>
-            <option value="Orthopedic">Orthopedic</option>
-            <option value="Formal">Formal</option>
-            <option value="Sneakers">Sneakers</option>
-          </select>
-        </div>
-        <button
-          type="submit"
-          className="w-full bg-gradient-to-r from-blue-400 to-purple-600 text-white py-2 px-4 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600"
+      {loggedIn ? (
+        <form
+          onSubmit={handleSubmit}
+          className="w-full max-w-md bg-gray-100 rounded-lg shadow-lg p-6"
         >
-          Add Item
-        </button>
-      </form>
+          <h2 className="text-2xl mb-4 text-center">Add New Shoe Item</h2>
+          <div className="mb-4">
+            <label
+              htmlFor="itemName"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Shoe Name:
+            </label>
+            <input
+              type="text"
+              id="itemName"
+              value={itemName}
+              onChange={(e) => setItemName(e.target.value)}
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="sizes"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Sizes Available:
+            </label>
+            <input
+              type="text"
+              id="sizes"
+              value={sizes}
+              onChange={(e) => setSizes(e.target.value)}
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="imageUrl"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Image URL:
+            </label>
+            <input
+              type="url"
+              id="imageUrl"
+              value={imageUrl}
+              onChange={(e) => setImageUrl(e.target.value)}
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="price"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Price:
+            </label>
+            <input
+              type="number"
+              id="price"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="gender"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Gender:
+            </label>
+            <select
+              id="gender"
+              value={gender}
+              onChange={(e) => setGender(e.target.value)}
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              required
+            >
+              <option value="">Select gender</option>
+              <option value="Men">Men</option>
+              <option value="Women">Women</option>
+              <option value="Kids">Kids</option>
+            </select>
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="category"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Category:
+            </label>
+            <select
+              id="Category"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              required
+            >
+              <option value="">Select Category</option>
+              <option value="Athletic">Athletic</option>
+              <option value="Casual">Casual</option>
+              <option value="Dress">Dress</option>
+              <option value="Boots">Boots</option>
+              <option value="Sandals">Sandals</option>
+              <option value="Specialty">Specialty</option>
+              <option value="Orthopedic">Orthopedic</option>
+              <option value="Formal">Formal</option>
+              <option value="Sneakers">Sneakers</option>
+            </select>
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-gradient-to-r from-blue-400 to-purple-600 text-white py-2 px-4 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600"
+          >
+            Add Item
+          </button>
+          <button
+            type="button"
+            onClick={handleLogout}
+            className="mt-4 w-full bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600 focus:outline-none focus:bg-red-600"
+          >
+            Logout
+          </button>
+        </form>
+      ) : (
+        <div className="text-center">
+          <p className="text-xl font-semibold mb-4 text-white">
+            You are not logged in.
+          </p>
+          <p
+            onClick={() => router.push("/AdminLogin")} // Using router for navigation
+            className="text-blue-500 hover:text-blue-700 transition duration-300 ease-in-out inline-block cursor-pointer"
+          >
+            Login as Admin
+          </p>
+        </div>
+      )}
     </div>
   );
 }
--- src/app/AdminLogin/page.tsx
+++ src/app/AdminLogin/page.tsx
@@ -3,6 +3,15 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { FaHome } from "react-icons/fa";
 import Link from "next/link";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  updateDoc,
+  doc,
+} from "firebase/firestore";
+import { FIRESTORE_DB } from "../../../firebase.config";
 
 export default function Adlogin() {
   const [email, setEmail] = useState("");
@@ -10,14 +19,64 @@ export default function Adlogin() {
   const [loggedIn, setLoggedIn] = useState(false);
   const router = useRouter();
 
-  const handleLogin = () => {
-    // Perform your login validation here
-    if (email === "lokesh@gmail.com" && pass === "sujalangi") {
-      setLoggedIn(true); // Set loggedIn to true if login is successful
-      router.push("/Admin"); // Navigate to Admin page if logged in
-    } else {
-      // Handle invalid login scenario (optional)
-      alert("Invalid email or password");
+  const nextChar = (char: any) => {
+    const code = char.charCodeAt(0);
+
+    // Handle lowercase letters
+    if (char >= "a" && char <= "z") {
+      return String.fromCharCode(((code - 97 + 1 + 26) % 26) + 97);
+    }
+
+    // Handle uppercase letters
+    if (char >= "A" && char <= "Z") {
+      return String.fromCharCode(((code - 65 + 1 + 26) % 26) + 65);
+    }
+
+    // Non-alphabetic characters remain unchanged
+    return char;
+  };
+
+  const converthashedpass = (password: string) => {
+    let newpass = "";
+    for (let i = 0; i < password.length; i++) {
+      newpass += nextChar(password[i]);
+    }
+    return newpass;
+  };
+
+  const handleLogin = async () => {
+    try {
+      // Query Firestore for admin credentials
+      const adminCredRef = collection(FIRESTORE_DB, "admincred");
+      const q = query(adminCredRef, where("username", "==", email));
+      const querySnapshot = await getDocs(q);
+
+      if (!querySnapshot.empty) {
+        const adminData = querySnapshot.docs[0].data();
+        const passwordCorrect = converthashedpass(pass) === adminData.password;
+
+        if (passwordCorrect) {
+          setLoggedIn(true); // Set loggedIn to true if login is successful
+
+          // Update Firestore to mark admin as logged in
+          const adminDocRef = doc(
+            FIRESTORE_DB,
+            "admincred",
+            querySnapshot.docs[0].id
+          );
+          await updateDoc(adminDocRef, {
+            loggedIn: true,
+          });
+
+          router.push("/Admin"); // Navigate to Admin page if logged in
+        } else {
+          alert("Invalid email or password");
+        }
+      } else {
+        alert("Invalid email or password");
+      }
+    } catch (error) {
+      console.error("Error logging in:", error);
     }
   };
 
@@ -40,7 +99,7 @@ export default function Adlogin() {
         <div className="flex items-center mb-4 ml-4 justify-center space-x-2">
           <h1 className="text-3xl">EShoe</h1>
           <img
-            src="https://img.freepik.com/premium-photo/single-continuous-line-drawing-safety-hiking-boots_804788-5097.jpg"
+            src="https://img.freepik.com/premium-photo/singlex-continuous-line-drawing-safety-hiking-boots_804788-5097.jpg"
             className="w-14 h-12"
             alt="Eshoe logo"
           />
