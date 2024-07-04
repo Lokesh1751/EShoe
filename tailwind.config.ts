3df006cf87cf7df179a2cf8de4244bb864c78d59
Lokesh Kumar <lokeshkumar@Lokeshs-MacBook-Pro.local>
2024-07-04 15:33:31 +0530

    changes done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,8 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/screen/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/HomePageComponents/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
